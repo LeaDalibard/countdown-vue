3457e60242bcf189f820d9151e5ae94fe245eaa0
Lea Dalibard <leadalibard@gmail.com>
2021-01-04 14:08:22 +0100

    Adding Counter.vue and imported in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-
+<Counter/>
   </div>
 </template>
 
 <script>
+
+import Counter from "@/components/Counter";
+
 export default {
   name: 'Home',
+  components: {Counter},
   props: {
     msg: String
   }
