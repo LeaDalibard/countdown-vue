df6019f1e642c9f8fdfdbce93ddcadd46aa00aa0
Lea Dalibard <leadalibard@gmail.com>
2021-01-04 15:40:35 +0100

    Saving updates

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <h1>Counter</h1>
-    {{ days }}
-    {{ hours }}
-    {{ minutes }}
-    {{ seconds }}
+    <p>Days : {{ days }}</p>
+    <p>Hours : {{ hours }}</p>
+    <p>Minutes :  {{ minutes }}</p>
+    <p>Seconds : {{ seconds }}</p>
+{{this.hoursInMs}}
+    {{this.daysInMs}}
   </div>
 </template>
 
@@ -16,7 +18,8 @@ export default {
       days: 0,
       hours: 0,
       minutes: 0,
-      seconds: 0
+      seconds: 0,
+      endDate:1609779600000,
     }
   },
   computed: {
@@ -24,15 +27,38 @@ export default {
       return 1000
     },
     minutesInMs: function () {
-      return 60*1000
+      return 60 * 1000
     },
     hoursInMs: function () {
-      return 60*60*1000
+      return 60 * 60 * 1000
     },
     daysInMs: function () {
-      return 36*60*60*1000
+      return 24 * 60 * 60 * 1000
     },
+  },
+  methods: {
+    showRemaining() {
+      var currentDate=new Date();
+      var diff= Math.abs(this.endDate-currentDate);
+
+      //days
+      this.days= Math.floor(diff/this.daysInMs);
+      diff-=this.days*this.daysInMs;
+
+      //hours
+      this.hours=Math.floor(diff / this.hoursInMs)%24 ;
+      diff-=this.hours*this.hoursInMs
+
+      //minutes
+      this.minutes=Math.floor(diff/this.minutesInMs)%60;
+      diff-=this.minutes*this.minutesInMs;
+
+      //seconds
+      this.seconds=Math.floor(diff/this.secondsInMs)%60;
+    }
+
   }
+
 }
 </script>
 
