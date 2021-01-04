4108cd04ea0b9a4fdd160886c608eb7f5bc157ba
Lea Dalibard <leadalibard@gmail.com>
2021-01-04 14:17:14 +0100

    Adding default data in script and show them in script

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
     <h1>Counter</h1>
+    {{days}}
+    {{hours}}
+    {{minutes}}
+    {{seconds}}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Counter'
+  name: 'Counter',
+  data: function() {
+    return {
+      days: 0,
+      hours:0,
+      minutes:0,
+      seconds:0
+    }
+  },
 }
 </script>
 
