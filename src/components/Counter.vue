4de577061dd3ce634829a154367828ffaa7115db
Lea Dalibard <leadalibard@gmail.com>
2021-01-04 14:50:00 +0100

    Adding computed properties

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,24 +1,38 @@
 <template>
   <div>
     <h1>Counter</h1>
-    {{days}}
-    {{hours}}
-    {{minutes}}
-    {{seconds}}
+    {{ days }}
+    {{ hours }}
+    {{ minutes }}
+    {{ seconds }}
   </div>
 </template>
 
 <script>
 export default {
   name: 'Counter',
-  data: function() {
+  data: function () {
     return {
       days: 0,
-      hours:0,
-      minutes:0,
-      seconds:0
+      hours: 0,
+      minutes: 0,
+      seconds: 0
     }
   },
+  computed: {
+    secondsInMs: function () {
+      return 1000
+    },
+    minutesInMs: function () {
+      return 60*1000
+    },
+    hoursInMs: function () {
+      return 60*60*1000
+    },
+    daysInMs: function () {
+      return 36*60*60*1000
+    },
+  }
 }
 </script>
 
